59c63c2311ec48e51bba02bbe6113ff76d654755
Akshay-gh <akshay01gh@gmail.com>
2023-09-07 20:26:00 +0530

    Phase 1 - K8S cluster automation

---
+++ ubuntu/vagrant/setup-hosts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+IFNAME=$1
+THISHOST=$2
+ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
+NETWORK=$(echo $ADDRESS | awk 'BEGIN {FS="."} ; { printf("%s.%s.%s", $1, $2, $3) }')
+sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
+
+# remove ubuntu-jammy entry
+sed -e '/^.*ubuntu-jammy.*/d' -i /etc/hosts
+sed -e "/^.*$2.*/d" -i /etc/hosts
+
+# Update /etc/hosts about other hosts
+cat >> /etc/hosts <<EOF
+127.0.0.1   localhost
+${NETWORK}.111  kubemaster
+${NETWORK}.201  kubenode01
+${NETWORK}.202  kubenode02
+EOF
+
+# Enable Root login, Password authenticaation
+sed -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' -i /etc/ssh/sshd_config
+sed -e 's/PasswordAuthentication no/PasswordAuthentication yes/' -i /etc/ssh/sshd_config
+systemctl restart ssh
