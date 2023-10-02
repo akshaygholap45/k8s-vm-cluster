80a816987616b5f2a5776fb3d509cdcef049c1bc
Akshay-gh <akshay01gh@gmail.com>
2023-10-02 17:46:36 +0530

    Updated the logic for service checker

--- ansible_playbooks/join_cluster.sh
+++ ansible_playbooks/join_cluster.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.111:6443 --token 27ph2o.l96fc7lx7yvbcytz --discovery-token-ca-cert-hash sha256:f69d9244e0c61bbab8ed6f0ac85c666f38aef73a88256f53e36ec3ba381cb9f5 
+kubeadm join 192.168.56.111:6443 --token hib7oq.73s6aif2kr2ylj0k --discovery-token-ca-cert-hash sha256:3b85fe7976f2915e0c74e1257b6eb15895283032014f6812d052e955be3288b9 
